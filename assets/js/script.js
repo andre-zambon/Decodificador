dc2ccaa5906eb0c707c8302d9bbf2ffa980e1057
andre-zambon <you@example.com>
2022-12-26 16:14:14 -0300

    codigo de codificação e ajustes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,146 +1,139 @@
 function executa () {
-    
-    mostraCardErro()
-    mostraCardMensagem()
+
+    mostraCards()
     criptografa()
-    
+    autoResize()
+    escreveMensagemCritpo()
+
 }
 
-function mostraCardErro() {
-    
+function resolve (){
+    mostraCards()
+    descriptografa()
+}
+
+function mostraCards (){
     let mensagemVazia = document.getElementById('mensagem').value;
-    if(mensagemVazia =='') {
-        document.getElementById('cardErro').style.display='block';
-    }else {
-        document.getElementById('cardErro').style.display='none';
+    let cardMensagemCripto = document.getElementById('cardResultado');
+    let cardErro = document.getElementById('cardErro');
+
+    if(mensagemVazia == ''){
+        cardErro.style.display='block';
+        cardMensagemCripto.style.display='none'
+    }
+    else if (mensagemVazia !== ''){
+        cardErro.style.display='none';
+        cardMensagemCripto.style.display='block'
     }
 }
 
-function mostraCardMensagem(){
-    let campoMensagemCripto = document.getElementById('mensagemCripto').value;
-    
-    if (campoMensagemCripto !== ''){
-        document.getElementById('cardResultado');
-    }
-}
 
 
+function descriptografa (){
+    var mensagemEntrada = document.getElementById('mensagem').value
+    var campoSaidaMensagem = document.getElementById('mensagemCripto')
+   
 
-function criptografa () {
-    
-    let mensagemEntrada = document.getElementById('mensagem').value;
+    var codigo = [['ai', 'a'], ['enter', 'e'], ['imes', 'i'], ['ober', 'o'], ['ufat', 'u']]
 
-    for (let i = 0; i < mensagemEntrada.length; i++) {
+    for (let i = 0; i < codigo.length; i++) {
+        if (mensagemEntrada.includes(codigo[i][0])){ 
+           mensagemEntrada = mensagemEntrada.replaceAll(codigo[i][0], codigo [i][1])
 
+        };
         
-            .replaceAll('a', 'ai')
-            .replaceAll('e', 'enter')
-            .replaceAll('i', 'imes')
-            .replaceAll('o', 'ober')
-            .replaceAll('u', 'ufat');
+        console.log(mensagemEntrada)
+        campoSaidaMensagem.innerHTML = mensagemEntrada
+
+    }
+}
+
+function criptografa(){
+    
+    var mensagemEntrada = document.getElementById('mensagem').value
+    var arrayEntrada = mensagemEntrada.split(''); //Divide a string em um array
+    
+    
+    arrayEntrada.forEach(function(item, i) { //percorre a array substituindo os valores. parametro item se refere a letra e o parametro i ao indice i
+        if(item == "a") {
+            arrayEntrada[i] = "ai";
+            
+        } else if(item == "e") {
+            arrayEntrada[i] = "enter";
+
+        } else if(item == "i") {
+            arrayEntrada[i] = "imes";
+            
+        } else if(item == "o") {
+            arrayEntrada[i] = "ober";
+            
+        } else if(item == "u") {
+            arrayEntrada[i] = "ufat";
         }
         
-    }
-    
-    
-
-
-
-
-
-
-
-
-
-
-
-
-// function criptografa() {
-
-//     //funciona errado
-    
-//     let mensagemEntrada = document.getElementById('mensagem').value;
+        mensagemCripto = arrayEntrada.join('') //Une a array em uma string
         
-//     let mensagemSaida = mensagemEntrada  
+        return mensagemCripto
+    })
+    
+}
+
+function escreveMensagemCritpo(){
+    var campoSaidaMensagem = document.getElementById('mensagemCripto')
+    campoSaidaMensagem.innerHTML = mensagemCripto
+    
+    console.log(mensagemCripto)
+}
+
+
+
+
+function autoResize(){
+    
+    objTextArea = document.getElementById('mensagemCripto');
+    while (objTextArea.scrollHeight > objTextArea.offsetHeight)
+    {
+        objTextArea.rows += 1;
+    }
+}
+
+
+
+
+
+
+
+// function criptografa () {
+//     var campoSaidaMensagem = document.getElementById('mensagemCripto')
+//     var mensagemEntrada = document.getElementById('mensagem').value
+    
 //     .replaceAll('a', 'ai')
 //     .replaceAll('e', 'enter')
 //     .replaceAll('i', 'imes')
 //     .replaceAll('o', 'ober')
 //     .replaceAll('u', 'ufat');
 
-//     console.log(mensagemSaida)
-        
-// }    
-
-
-// let array = mensagemEntrada.split('')
-// let reg = /[a&e&i&o&u]/gmi;
-// let index = mensagemEntrada.match(reg);
-
-
-
-    // for (let i = 0; i < mensagemEntrada.length; i++) {
-        //     let letra = mensagemEntrada[i];
-        
-        //    console.log(letra)
-        // }
-        
-        
-        
-        
-    
-        
-//     console.log(mensagemEntrada.length)
-    
-//     // for (let i = 0; i < array.length; i++) {
-        
-        
-    
-    //     // }
-    
-    
-    
-    
-    
-    
-    
-    // for (let i = 0; i < index.length; i++) {
-        // if (index[i] == 'a'){
-        //     index[i] = 'ai'
-        // }
-        // if (index[i] == 'e'){
-            //     index[i] = 'enter'
-            // }
-            // if (index[i] == 'i'){
-                //     index[i] = 'imes'
-                // }
-                // if (index[i] == 'o'){
-        //     index[i] = 'ober'
-        // }
-        // if (index[i] == 'u'){
-            //     index[i] = 'ufat'
-        // }
-      
-        
-    // } 
-    
-    
-    // mensagemSaida = mensagemEntrada.replaceAll("a", "ai");
-    
-    
-    // mensagemCripto.innerHTML = mensagemSaida;
-    
-    // }
-    
-    
-    
-    // function converte () {
-        
+//     campoSaidaMensagem.innerHTML = mensagemEntrada
+//     console.log(mensagemEntrada)
+            
 // }
+        
+function botaoCopiar(){
+
+    var textoCopiado = document.getElementById('mensagemCripto').value;
+    navigator.clipboard.writeText(textoCopiado);
+
+}  
+    
+    
+    
 
 
-// A letra "a" é convertida para "ai"
-// A letra "e" é convertida para "enter"
-// A letra "i" é convertida para "imes"
-// A letra "o" é convertida para "ober"
-// A letra "u" é convertida para "ufat"
+
+
+
+
+
+
+
+
